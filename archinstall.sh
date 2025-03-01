88259a32695c823aa647d68a965fff69e4656906
Jose Manuel <jsegrod1512@g.educaand.es>
2025-03-01 22:54:30 +0100

    EOF correccion

--- archinstall.sh
+++ archinstall.sh
@@ -108,7 +108,7 @@ arch-chroot /mnt /bin/bash <<EOF
 
     # 6.11. Crear configuración personalizada de i3 para evitar el prompt inicial
     su - $USER -c 'mkdir -p ~/.config/i3'
-    su - $USER -c "cat <<'EOT' > ~/.config/i3/config
+    su - $USER -c "cat <<EOF > ~/.config/i3/config
     ############################################
     #       Configuración predeterminada i3
     ############################################
@@ -172,7 +172,7 @@ arch-chroot /mnt /bin/bash <<EOF
 
     # Establecer wallpaper con feh (opcional)
     # exec --no-startup-id feh --bg-scale /ruta/a/tu/wallpaper.jpg
-    EOT"
+    EOF
 EOF
 
 echo "===== [7/7] FINALIZANDO INSTALACION ====="
