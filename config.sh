8701138edd2ef3fa4a7588259c299d324e0e5c05
Jose Manuel <jsegrod1512@g.educaand.es>
2025-03-05 14:06:27 +0100

    config.sh correccion

--- config.sh
+++ config.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Asegurar permisos de ejecución
-chmod +x ./scripts/*.sh
-
-chmod +x ./*.sh
+chmod +x ./*
 
 SCRIPTS=("dmenu_config.sh" "i3install.sh" "polybarinstall.sh")
 
 # Ejecutar los scripts en el orden especificado
 for script in "${SCRIPTS[@]}"; do
-    if [[ -f "./$script" ]]; then
+    if [[ -f "$script" ]]; then
         echo "Ejecutando: $script"
-        bash "./$script"
+        bash "$script"
     else
         echo "Advertencia: No se encontró $script"
     fi
@@ -20,15 +18,14 @@ done
 # Mover los scripts a su ubicación final en ~/.config/scripts
 mkdir -p ~/.config/scripts
 cp ./* ~/.config/scripts/
-chmod +x ~/.config/scripts/*.sh
+chmod +x ~/.config/scripts/*
 
-# write out current crontab
-crontab -l > mycron
-# echo new cron into cron file
-echo "0 3 * * 1 $HOME/.config/scripts/backup_config.sh" >> mycron
-# install new cron file
-crontab mycron
-rm mycron
+# Configurar el backup en cron
+BACKUP_SCRIPT="$HOME/.config/scripts/backup_config.sh"
+CRON_JOB="0 3 * * 1 $BACKUP_SCRIPT > /dev/null 2>&1"
+
+# Verificar si ya existe el cronjob y agregarlo si no está
+(crontab -l | grep -Fq "$BACKUP_SCRIPT") || (crontab -l; echo "$CRON_JOB") | crontab -
 
 echo "Scripts ejecutados!"
 echo "--------------------"
